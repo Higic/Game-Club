d4ee96641d3b5a55ab1934aaa20e977290d5c130
Higic <onni.alasaari@gmail.com>
2024-01-30 22:29:28 +0200

    added new page

---
+++ game-club-next/src/app/looking-for-games/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Looking for games</h1>
+        <p>Here you can look for games and people to play with.</p>
+      </div>
+    )
+  }
