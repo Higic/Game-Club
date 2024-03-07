a1de5a015debf105b3fa15350f67c52b97fe112f
Edvard Nivala <edvard.nivala@metropolia.fi>
2024-03-08 01:10:54 +0200

    Experimenting user registration
    with REGISTER_MUTATION and useMutation hook. Produces errors @ localhost:3000/register.  To be continued..

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,29 @@
 "use client";
 
 import { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+
+// experimenting with register mutation variable
+// testing functionality, doesn't work yet
+// goes with useMutation hook
+const REGISTER_MUTATION = gql`
+  mutation Register($user: UserInput!) {
+    register(user: $user) {
+      user {
+        id
+        user_name
+        email
+      }
+    }
+  }
+`;
 
 export default function Register() {
   let MIN_USERNAME_LENGTH = 4;
   let MAX_USERNAME_LENGTH = 20;
+
+  // useMutation hook
+  const [registerMutation, { loading: registerLoading, error: registerError }] = useMutation(REGISTER_MUTATION);
 
   const [formData, setFormData] = useState({
     username: "",
@@ -21,7 +40,7 @@ export default function Register() {
     });
   };
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
     if (formData.password !== formData.confirmPassword) {
       alert("Passwords do not match");
@@ -49,6 +68,13 @@ export default function Register() {
     }
     console.log(formData);
     // send to api
+    // experimenting. Try to register user with registerMutation, doesn't work yet
+    try {
+      const result = await registerMutation({ variables: { user: formData } });
+      console.log(result.data.register);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
