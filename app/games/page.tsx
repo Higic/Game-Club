0fc2952843f1e1fce9f07f4d7c8c8473bde458ac
Edvard Nivala <edvard.nivala@metropolia.fi>
2024-03-11 22:47:30 +0200

    Slight adjustments

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,7 +1,12 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>Would you rather have games?!</h1>
+      <div>
+        <h1>Would you rather have games?!</h1>
+      </div>
+      <div>
+        <h2>Most played games today</h2>
+      </div>
     </div>
   )
 }
