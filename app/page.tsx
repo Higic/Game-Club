2170a15a310be5dcd1cadb7fda0470a071eb7857
Edvard Nivala <edvard.nivala@metropolia.fi>
2024-03-12 17:28:05 +0200

    login / register buttons for dev convenience

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
+"use client";
+
 import connectMongo from "@/utils/db";
+import { useRouter } from "next/navigation";
 
 /**
  * This is the home page of the app
  */
 export default function HomePage() {
+  const router = useRouter();
   
   return (
     <div className="homePageIntro">
         <h1>Welcome to Game Club!</h1>
         <p>Use the search bar to find the game you are looking for</p>
+        <div>
+          <button onClick={() => router.push(`/register`)}>
+            Register
+          </button>
+          <button onClick={() => router.push(`/login`)}>
+            Login
+          </button>
+        </div>
     </div>
   )
 }
