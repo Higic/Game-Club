5f8219f272774c9a006d639f6cd853757113b332
Higic <onni.alasaari@gmail.com>
2024-03-13 01:48:48 +0200

    removed roles

--- functions/authorize.ts
+++ functions/authorize.ts
@@ -1,7 +1,7 @@
 import { GraphQLError } from "graphql";
 import MyContext from "../types/MyContext";
 
-const isLoggedIn = (context: MyContext): void => {
+const isLoggedIn = (context: MyContext): void => { 
     if (!context.userdata) {
         throw new GraphQLError('You are not authenticated', {
             extensions: {
@@ -12,24 +12,6 @@ const isLoggedIn = (context: MyContext): void => {
             },
         });
     }
-};
+    };
 
-/**
- * Check if the user is an admin
- * @param context
- */
-const isAdmin = (context: MyContext): void => {
-    isLoggedIn(context);
-    if (context.userdata && context.userdata.user.role !== 'admin') {
-        throw new GraphQLError('You are not authorized to do this', {
-            extensions: {
-                code: 'UNAUTHORIZED',
-                http: {
-                    status: 401,
-                },
-            },
-        });
-    }
-};
-
-export { isLoggedIn, isAdmin };
+export { isLoggedIn };
