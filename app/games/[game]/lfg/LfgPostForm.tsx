0098fa50e112e7c12c5dc8c5c5359e78e41f84f9
Edvard Nivala <edvard.nivala@metropolia.fi>
2024-03-13 20:03:18 +0200

    LFG Tab works
    .. with a hard coded game for now.

--- app/games/[game]/lfg/LfgPostForm.tsx
+++ app/games/[game]/lfg/LfgPostForm.tsx
@@ -1,15 +1,41 @@
 "use client";
 
+import { CREATE_LFG_MUTATION } from "@/app/api/graphql/mutations/lfgMutations";
+import GetLoggedInUser from "@/components/getLoggedInUser";
+import { useMutation } from "@apollo/client";
 import { useState } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function LfgPostForm() {
     const [text, setText] = useState("");
+    const [createLfgMutation, { loading: createLfgLoading, error: createLfgError }] = useMutation(CREATE_LFG_MUTATION);
+
+    const author = GetLoggedInUser();
+    const token = Cookies.get("token");
+
+    if (!author || !token) {
+        console.log("No user logged in")
+        const router = useRouter(); // redirect user to login page
+    }
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
-        console.log("LfgPostForm .  Text: " + text);
-        //Do Database Send stuff
-    }
+        const formData = {
+            text: text,
+            game: "Metal Gear Rising 2 - Revengeance", // Hardcoded game for now
+            author: author?.user_name, // Add null check
+        };
+
+        // Try the mutation
+        try {
+            const result = await createLfgMutation({ variables: { input: formData } });
+            alert("LFG post created!");
+            if (result) { console.log("LFG post created!", result)}
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <div>
@@ -17,15 +43,20 @@ export default function LfgPostForm() {
                 <h2>Make a LFG post:</h2>
                 <textarea
                     rows={4}
-                    maxLength={100}
+                    maxLength={200}
                     value={text}
                     placeholder="Make a post..."
                     onChange={(e) => setText(e.target.value)}>
                 </textarea>
+                <div>
+                    <span style={{ fontSize: "12px" }}>Characters remaining: {200 - text.length}</span>
+                </div>
                 <input
                     type="submit"
-                    value="Submit">
-                </input>
+                    value="Submit"
+                    disabled={createLfgLoading}
+                />
+                {createLfgError && <p>Error creating LFG: {createLfgError.message}</p>}
             </form>
         </div>
     );
