eddafd6df078b4034c7a1cda7a9c685012628c2f
Higic <onni.alasaari@gmail.com>
2024-03-14 14:45:35 +0200

    fixed import

--- app/api/graphql/resolvers/index.ts
+++ app/api/graphql/resolvers/index.ts
@@ -1,9 +1,9 @@
-import lfgResolver from "./lfgResolver";
 import forumCommentResolver from "./forumCommentResolver";
 import forumPostResolver from "./forumPostResolver";
 import reviewResolver from "./reviewResolver";
 import userResolver from "./userResolver";
 import gameResolver from "./gameResolver";
+import lfgResolver from "./LFGResolver";
 
 /**
  * This is the main resolver file. It connects all the resolvers for the different api's.
