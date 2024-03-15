78da8083800ee749a3ecef05c6a34dbe4fdb712d
Higic <onni.alasaari@gmail.com>
2024-03-15 11:46:53 +0200

    updated nextconfig

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     env: {
         JWT_SECRET: "supersecret",
-        AUTH_URL: "game-club-auth-server.azurewebsites.net",
+        AUTH_URL: "https://game-club-auth-server.azurewebsites.net",
         DATABSE_URL: "mongodb+srv://tapiojh:HSJ1cKTvvA1qQfgM@game-club.405k6xl.mongodb.net/?retryWrites=true&w=majority&appName=game-club",
     }
 }
