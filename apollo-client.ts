c0412385cfc0774f70d44afb1d3d656dc2d00b96
Higic <onni.alasaari@gmail.com>
2024-03-15 11:00:36 +0200

    changed to env value

--- apollo-client.ts
+++ apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: "http://localhost:3000/api/graphql",
+    uri: process.env.APOLLO_URI,
     cache: new InMemoryCache(),
   });
 };
