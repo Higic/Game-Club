ddfc8ddf0a2518bfa2f925f4e90b361b64901366
Higic <onni.alasaari@gmail.com>
2024-03-15 11:07:48 +0200

    added logs

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
+  console.log(process.env.APOLLO_URI);
   return new ApolloClient({
     uri: process.env.APOLLO_URI,
     cache: new InMemoryCache(),
