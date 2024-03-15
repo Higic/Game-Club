87c23cd8618ea80c3c944b5b5f8cd66de1a4258f
Higic <onni.alasaari@gmail.com>
2024-03-15 12:07:49 +0200

    updated apollo address

--- apollo-client.ts
+++ apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: "https://game-club-two.vercel.app/api/graphql",
+    uri: "https://game-club-9jk94e43n-onnis-projects-fd36d217.vercel.app/api/graphql",
     cache: new InMemoryCache(),
   });
 };
--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        JWT_SECRET: "supersecret",
-        AUTH_URL: "https://game-club-auth-server.azurewebsites.net",
-        DATABSE_URL: "mongodb+srv://tapiojh:HSJ1cKTvvA1qQfgM@game-club.405k6xl.mongodb.net/?retryWrites=true&w=majority&appName=game-club",
-    }
 }
 
 module.exports = nextConfig
